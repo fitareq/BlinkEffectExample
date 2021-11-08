8a6deda8f26c65a8d5fb4844da8189581d4b6d4a
fitareq <fitareq@gmail.com>
2021-11-08 17:02:35 +0600

    initial commit

--- app/src/main/java/com/example/blinkeffectexample/MainActivity.kt
+++ app/src/main/java/com/example/blinkeffectexample/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.blinkeffectexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.blinklibrary.BlinkEffect
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView = findViewById<TextView>(R.id.textview)
+
+        BlinkEffect.blink(textView)
 
     }
 }
